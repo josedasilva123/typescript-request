cfbff0ce87430c76fa6124a996346f77250a7bbf
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-07-29 22:58:36 -0300

    body params and query

--- aula/src/index.ts
+++ aula/src/index.ts
@@ -1,8 +1,34 @@
 import { productsApi } from "./api/products.api";
 
-async function request(){
-    const response = await productsApi.get("/products");
+async function getProducts(){
+    const response = await productsApi.get("/products", {
+        params: {
+            search: "note"
+        }
+    });
+
     console.log(response.data);
 }
 
-request();
+async function getOneProduct(){
+    const response = await productsApi.get("/products/2");
+
+    console.log(response.data);
+}
+
+getOneProduct();
+
+async function createProduct(){
+    const body = {
+        name: "Notebook",
+        description: "Notebook bacana",
+        price: 4000
+    }
+
+    // POST, PUT e PATCH
+    const response = await productsApi.post("/products", body);
+
+    console.log(response.data);
+}
+
+//createProduct();
