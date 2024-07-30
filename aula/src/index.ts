00e76c3babbdeebc274cae6d8ee8b1c4f23976e7
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-07-29 22:25:52 -0300

    axios request

--- aula/src/index.ts
+++ aula/src/index.ts
@@ -1,7 +1,8 @@
+import axios from "axios";
+
 async function request(){
-    const response = await fetch("http://localhost:3001/products");
-    const json = await response.json();
-    console.log(json);
+    const response = await axios.get("http://localhost:3001/products");
+    console.log(response.data);
 }
 
 request();
